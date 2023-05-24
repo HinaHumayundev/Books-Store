5616f7b3eeb2ac4970af748051ddea87d35457bf
hina.humayun <hhumayun@ssidecisions.com>
2023-05-24 03:48:36 +0200

    add frontend endpoint implementation

---
+++ frontend/frontend/src/components/Shark.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <p>{{ msg }}</p>
+    </div>
+</template>  
+
+<script>
+import axios from 'axios';
+  export default{
+    name: 'SharkComponent',
+    data() {
+      return {
+        msg: ""
+      };
+    },
+    methods :
+    {
+      getResponse() {
+        const path = 'http://localhost:5000/shark';
+        axios.get(path)
+        .then((res)=>{
+          console.log(res.data)
+          this.msg = res.data;
+        })
+        .catch((err)=>{
+          console.log(err.data);
+        });
+      }
+    },
+    created() {
+      this.getResponse();
+
+    }
+  };
+</script>
