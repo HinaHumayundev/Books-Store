d6563e806fa308a127a9fddbb22575f53902914d
hina.humayun <hhumayun@ssidecisions.com>
2023-05-24 03:30:45 +0200

    add endpoint

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,11 @@ CORS(app, resources={r"/*":{'origins':"*"}})
 
 #hello world route
 
-@app.route('/', methods=['GET'])
-def greetings():
-    return("Hello", "world!")
+
+
+@app.route('/shark', methods=['GET'])
+def shark():
+    return("Shark!")
 
 if __name__ == '__main__':
     app.run(debug=True)
