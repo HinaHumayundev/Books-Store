2e75a2b2939e3d10da605917741d73e117bbb3da
hina.humayun <hhumayun@ssidecisions.com>
2023-05-25 19:25:04 +0200

    remove error

--- backend/main.py
+++ backend/main.py
@@ -80,18 +80,6 @@ def all_books():
 
 
 #book update and delete route handler
-@app.route('books/<book_id>', methods=['PUT'])
-def update_book(book_id):
-    response_object ={'status':'success'}
-    if request.method == 'PUT':
-        post_data = request.get_json()
-        remove_book(book_id)
-        BOOKS.append({
-            'BookId':uuid.uuid4().hex,
-            'title': post_data.get('title'),
-            'genre': post_data.get('genre'),
-            'read': post_data.get('read')  })
-        response_object['message'] = 'Book is updated Successfully!'
 
 def remove_book(book_id):
     for book in BOOKS:
